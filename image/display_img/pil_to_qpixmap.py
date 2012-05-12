5d214773a5e64a1c3d05851c8c2d4dc9ebc5d851
Shuge Lee <shuge.lee@gmail.com>
2012-05-12 17:28:12 +0800

    small changed

--- image/display_img/pil_to_qpixmap.py
+++ image/display_img/pil_to_qpixmap.py
@@ -21,9 +21,19 @@ from PySide import QtGui
 PWD = os.path.dirname(os.path.realpath(__file__))
 parent_path = os.path.dirname(PWD)
 IM_RES_PATH = os.path.join(os.path.dirname(parent_path), "image_resources")
-
-
 file_path = os.path.join(IM_RES_PATH, "captcha.jpg")
+
+
+def pil2pixmap(file_path):
+    im = Image.open(fp = file_path)
+    if im.mode == "RGB":
+        pass
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    data = im.tostring('raw', "RGBA")
+    qim = QtGui.QImage(data, im.size[0], im.size[1], QtGui.QImage.Format_ARGB32)
+    pixmap = QtGui.QPixmap.fromImage(qim)
+    return pixmap
 
 
 app = QtGui.QApplication(sys.argv)
@@ -32,16 +42,7 @@ x, y, w, h = 100, 100, 200, 200
 label = QtGui.QLabel()
 label.setGeometry(x, y, w, h)
 
-
-im = Image.open(fp = file_path)
-if im.mode == "RGB":
-    pass
-elif im.mode == "L":
-    im = im.convert("RGBA")
-data = im.tostring('raw', "RGBA")
-qim = QtGui.QImage(data, im.size[0], im.size[1], QtGui.QImage.Format_ARGB32)
-pixmap = QtGui.QPixmap.fromImage(qim)
-
+pixmap = pil2pixmap(file_path)
 label.setPixmap(pixmap)
 
 label.move(10, 10)
